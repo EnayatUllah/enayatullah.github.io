10d1651b20de0d5a3c39046802b9cb505cc2cad4
Enayat Ullah <md.enayatullah@gmail.com>
2017-01-09 15:37:31 +0530

    minor corrections jan 2017

---
+++ push_iitk.sh
@@ -0,0 +1,5 @@
+scp cv.pdf enayat@webhome.cc.iitk.ac.in:/www/enayat/www/
+scp -rv img enayat@webhome.cc.iitk.ac.in:/www/enayat/www/
+scp -rv files enayat@webhome.cc.iitk.ac.in:/www/enayat/www/
+scp index.html enayat@webhome.cc.iitk.ac.in:/www/enayat/www/
+scp resume.pdf enayat@webhome.cc.iitk.ac.in:/www/enayat/www/
