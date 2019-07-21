bdcac58260a13f8e8444d804bb955dbbdd1f9340
Enayat Ullah <md.enayatullah@gmail.com>
2019-07-21 18:43:36 -0400

    grad website

--- push_iitk.sh
+++
@@ -1,5 +0,0 @@
-scp cv.pdf enayat@webhome.cc.iitk.ac.in:/www/enayat/www/
-scp -rv img enayat@webhome.cc.iitk.ac.in:/www/enayat/www/
-scp -rv files enayat@webhome.cc.iitk.ac.in:/www/enayat/www/
-scp index.html enayat@webhome.cc.iitk.ac.in:/www/enayat/www/
-scp resume.pdf enayat@webhome.cc.iitk.ac.in:/www/enayat/www/
